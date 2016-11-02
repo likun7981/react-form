4d3f4e400ae24150fd3b26ef38437fea5cea111f
likun <kun.li04@ele.me>
2016-11-02 19:56:53 +0800

    add some .config

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+const eslintrc = {
+	"extends":"airbnb",
+	"env":{
+		"browser":true,
+		"node":true
+	},
+	"parser":"babel-eslint",
+	"rules":{
+		"comma-dangle":["error","never"],
+		"react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+		"react/forbid-prop-types":[0]
+	}
+}
+
+module.exports = eslintrc;
